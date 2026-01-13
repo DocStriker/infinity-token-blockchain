f0013c1b9c1c20c0d818eb3fba2a0ebd03cfc8f5
J. Álaf <122956015+DocStriker@users.noreply.github.com>
2026-01-13 05:10:34 +0000

    feat - initial commit

---
+++ contracts/InfinityToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.33;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/security/Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/access/Ownable.sol";
+
+contract InfinityToken is ERC20, ERC20Burnable, Pausable, Ownable {
+
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10 ** 12;
+
+    constructor(address initialOwner) ERC20("InfinityToken", "ITK") {
+        _transferOwnership(initialOwner);
+        _mint(initialOwner, INITIAL_SUPPLY);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}

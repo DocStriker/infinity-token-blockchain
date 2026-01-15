19d44e38b2f2f6db30d1cc158ca0a61b1855da5b
J. Álaf <122956015+DocStriker@users.noreply.github.com>
2026-01-15 07:02:36 +0000

    fix - package updates and etherscan call fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,9 @@
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
 import "dotenv/config";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
-  solidity: "0.8.33",
+  solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
@@ -12,8 +11,9 @@ export default {
     },
   },
   etherscan: {
-    apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY,
-    },
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
+  sourcify: {
+    enabled: true
+  }
 };
